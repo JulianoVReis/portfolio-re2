237b741e2c7c05a1dda71c985050fbd6176b071d
Juliano Reis <julianovreis@gmail.com>
2025-06-10 17:20:22 -0300

    update portfolio

---
+++ script.js
@@ -0,0 +1,58 @@
+let whatsapp = document.querySelector('.whatsapp')
+let gmail = document.querySelector('.gmail')
+let info1img = document.querySelector('.info-1-img')
+let info2txt = document.querySelector('.txt-info-2')
+let img = document.createElement('img')
+info1img.appendChild(img)
+
+whatsapp.addEventListener('click', () => {
+	if (info1img.style.display === 'flex' && img.src.includes('whatsapp.png')) {
+    info1img.style.display = 'none'
+    info2txt.style.display = 'none'
+  } else {
+    info1img.style.display = 'flex'
+    img.src = 'img/whatsapp.png'
+    info2txt.style.display = 'block'
+		typeWriterInfo('<span style="color: green;">WhatsApp</span>: (51) 989199543')
+  }
+})
+
+gmail.addEventListener('click', () => {
+	if (info1img.style.display === 'flex' && img.src.includes('gmail.webp')) {
+    info1img.style.display = 'none'
+    info2txt.style.display = 'none'
+  } else {
+    info1img.style.display = 'flex'
+    img.src = 'img/gmail.webp'
+    info2txt.style.display = 'block'
+		typeWriterInfo('<span style="color: green;">Gmail</span>: julianovreis@gmail.com')
+  }
+})
+
+document.addEventListener('click', (e) => {
+  if (
+    !whatsapp.contains(e.target) &&
+    !gmail.contains(e.target) &&
+    !info1img.contains(e.target)
+  ) {
+    info1img.style.display = 'none';
+    info2txt.style.display = 'none';
+  }
+})
+
+let typeitInstance = null
+
+function typeWriterInfo(texto) {
+  if (typeitInstance) {
+    typeitInstance.destroy()
+    document.querySelector('#info2txt').innerHTML = ''
+  }
+
+  typeitInstance = new TypeIt("#info2txt", {
+    strings: texto,
+    speed: 50,
+    loop: false,
+    waitUntilVisible: true,
+		cursor: false
+  }).go()
+}
