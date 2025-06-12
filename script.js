52d0eb721f7522664ca26af9e18ab3561892a99d
Juliano Reis <julianovreis@gmail.com>
2025-06-12 14:05:04 -0300

    update portfolio

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 // inicio da animacao de clique do inventario
 let whatsapp = document.querySelector('.whatsapp')
 let gmail = document.querySelector('.gmail')
@@ -10,6 +11,9 @@ let info1img = document.querySelector('.info-1-img')
 let info1 = document.querySelector('.info-1')
 let info1Span = document.querySelector('.info-1 span')
 let info2txt = document.querySelector('#info2txt')
+let carrossel = document.querySelector('.carousel')
+let projetos = document.querySelector('#projetos')
+
 let img = document.createElement('img')
 info1img.appendChild(img)
 
@@ -20,48 +24,54 @@ let originalBackgroundColor = info1Styles.backgroundColor
 // fim estilos originais
 
 whatsapp.addEventListener('click', () => {
-	modalSkills.style.display = 'none'
-	info1.style.borderImage = originalBorderImage
-	info1.style.backgroundColor = originalBackgroundColor
-	info1Span.style.display = 'block'
-	if (info1img.style.display === 'flex' && img.src.includes('whatsapp.png')) {
+  carrossel.style.display = 'none'
+  modalSkills.style.display = 'none'
+  info1.style.borderImage = originalBorderImage
+  info1.style.backgroundColor = originalBackgroundColor
+  info1Span.style.display = 'block'
+
+  if (info1img.style.display === 'flex' && img.src.includes('whatsapp.png')) {
     info1img.style.display = 'none'
     info2txt.style.display = 'none'
   } else {
     info1img.style.display = 'flex'
     img.src = 'img/whatsapp.png'
     info2txt.style.display = 'block'
-		typeWriterInfo('<span style="color: #19E428;">WhatsApp</span><br>(51) 989199543')
+    typeWriterInfo('<span style="color: #19E428;">WhatsApp</span><br>(51) 989199543')
   }
 })
 
 gmail.addEventListener('click', () => {
-	modalSkills.style.display = 'none'
-	info1.style.borderImage = originalBorderImage
-	info1.style.backgroundColor = originalBackgroundColor
-	info1Span.style.display = 'block'
-	if (info1img.style.display === 'flex' && img.src.includes('gmail.webp')) {
+  carrossel.style.display = 'none'
+  modalSkills.style.display = 'none'
+  info1.style.borderImage = originalBorderImage
+  info1.style.backgroundColor = originalBackgroundColor
+  info1Span.style.display = 'block'
+
+  if (info1img.style.display === 'flex' && img.src.includes('gmail.webp')) {
     info1img.style.display = 'none'
     info2txt.style.display = 'none'
   } else {
     info1img.style.display = 'flex'
     img.src = 'img/gmail.webp'
     info2txt.style.display = 'block'
-		typeWriterInfo('<span style="color: #19E428;">Gmail</span><br>julianovreis@gmail.com')
+    typeWriterInfo('<span style="color: #19E428;">Gmail</span><br>julianovreis@gmail.com')
   }
 })
 
 skills.addEventListener('click', () => {
-	info1.style.borderImage = 'none'
-	info1.style.backgroundColor = '#07081F'
-	info1img.style.display = 'none'
-	info1Span.style.display = 'none'
-	info2txt.style.display = 'none'
-	if (modalSkills.style.display === 'block') {
-		modalSkills.style.display = 'none'
-	} else {
-		modalSkills.style.display = 'block'
-	}
+  carrossel.style.display = 'none'
+  info1.style.borderImage = 'none'
+  info1.style.backgroundColor = '#07081F'
+  info1img.style.display = 'none'
+  info1Span.style.display = 'none'
+  info2txt.style.display = 'none'
+
+  if (modalSkills.style.display === 'block') {
+    modalSkills.style.display = 'none'
+  } else {
+    modalSkills.style.display = 'block'
+  }
 })
 
 document.addEventListener('click', (e) => {
@@ -90,25 +100,25 @@ function typeWriterInfo(texto) {
     speed: 50,
     loop: false,
     waitUntilVisible: true,
-		cursor: false
+    cursor: false
   }).go()
 }
 // fim typewriter
 
 // inicio do modal das skills
 modalSkillsN1.addEventListener('click', () => {
-	info2txt.style.display = 'block'
-	typeWriterInfo('<span style="color: #19E428;">HTML5/CSS3</span><br>Curso em Vídeo - 40h<br>Origamid - 23h')
+  info2txt.style.display = 'block'
+  typeWriterInfo('<span style="color: #19E428;">HTML5/CSS3</span><br>Curso em Vídeo - 40h<br>Origamid - 23h')
 })
 
 modalSkillsN2.addEventListener('click', () => {
-	info2txt.style.display = 'block'
-	typeWriterInfo('<span style="color: #19E428;">Algoritmos</span><br>Curso em Vídeo - 40h')
+  info2txt.style.display = 'block'
+  typeWriterInfo('<span style="color: #19E428;">Algoritmos</span><br>Curso em Vídeo - 40h')
 })
 
 modalSkillsN3.addEventListener('click', () => {
-	info2txt.style.display = 'block'
-	typeWriterInfo('<span style="color: #19E428;">JavaScript</span><br>Curso em Vídeo - 40h<br>Origamid - 37h')
+  info2txt.style.display = 'block'
+  typeWriterInfo('<span style="color: #19E428;">JavaScript</span><br>Curso em Vídeo - 40h<br>Origamid - 37h')
 })
 
 document.addEventListener('click', (e) => {
@@ -118,7 +128,8 @@ document.addEventListener('click', (e) => {
     modalSkillsN2,
     modalSkillsN3,
     whatsapp,
-    gmail
+    gmail,
+    skills
   ]
 
   let clicouFora = !ignorarClique.some(el => el.contains(e.target))
@@ -127,3 +138,140 @@ document.addEventListener('click', (e) => {
   }
 })
 // fim do modal das skills
+
+// inicio menu navegacao
+projetos.addEventListener('click', () => {
+  const isVisible = carrossel.style.display === 'block'
+
+  if (isVisible) {
+    carrossel.style.display = 'none'
+  } else {
+    carrossel.style.display = 'block'
+    info1.style.display = 'flex'
+    info1img.style.display = 'none'
+    info2txt.style.display = 'none'
+    info1Span.style.display = 'block'
+    modalSkills.style.display = 'none'
+    info1.style.borderImage = originalBorderImage
+    info1.style.backgroundColor = originalBackgroundColor
+  }
+})
+// fim menu navegacao
+
+// inicio carrossel
+'use strict'
+
+class Carousel {
+  constructor(el) {
+    this.el = el
+    this.carouselOptions = ['previous', 'next']
+    this.carouselData = [
+  { id: '1', src: 'img/port_re2.png', title: 'Portfólio RE2', link: '' },
+  { id: '2', src: 'img/bikcraft.png', link: '' },
+  { id: '3', src: 'img/animais-fantasticos.png', link: '' },
+  { id: '4', src: 'img/pokedex.png', title: 'Pokédex JavaScript', link: 'https://julianovreis.github.io/pokedex/' },
+  { id: '5', src: 'img/projeto-portfolio.png', link: '' }
+]
+
+
+
+    this.carouselInView = [1, 2, 3, 4, 5]
+    this.carouselContainer
+  }
+
+  mounted() {
+    this.setupCarousel()
+  }
+
+  setupCarousel() {
+    const container = document.createElement('div')
+    const controls = document.createElement('div')
+
+    this.el.append(container, controls)
+    container.className = 'carousel-container'
+    controls.className = 'carousel-controls'
+
+    this.carouselData.forEach((item, index) => {
+      const carouselItemWrapper = document.createElement('div')
+      const carouselItem = document.createElement('img')
+      const caption = document.createElement('div')
+
+      carouselItemWrapper.className = `carousel-item carousel-item-${index + 1}`
+      carouselItem.src = item.src
+      carouselItem.setAttribute('loading', 'lazy')
+      carouselItem.setAttribute('data-index', `${index + 1}`)
+
+      caption.className = 'carousel-caption'
+caption.innerHTML = `<a href="${item.link}" target="_blank">${item.title}</a>`
+
+      carouselItemWrapper.appendChild(carouselItem)
+      carouselItemWrapper.appendChild(caption)
+      container.append(carouselItemWrapper)
+    })
+
+    this.carouselOptions.forEach((option) => {
+      const btn = document.createElement('button')
+      const axSpan = document.createElement('span')
+      axSpan.innerText = option
+      axSpan.className = 'ax-hidden'
+      btn.append(axSpan)
+      btn.className = `carousel-control carousel-control-${option}`
+      btn.setAttribute('data-name', option)
+      controls.append(btn)
+    })
+
+    this.setControls([...controls.children])
+    this.carouselContainer = container
+  }
+
+  setControls(controls) {
+    controls.forEach(control => {
+      control.onclick = (event) => {
+        event.preventDefault()
+        this.controlManager(control.dataset.name)
+      }
+    })
+  }
+
+  controlManager(control) {
+    if (control === 'previous') return this.previous()
+    if (control === 'next') return this.next()
+  }
+
+  previous() {
+    this.carouselData.unshift(this.carouselData.pop())
+    this.carouselInView.push(this.carouselInView.shift())
+
+    this.carouselInView.forEach((item, index) => {
+      this.carouselContainer.children[index].className = `carousel-item carousel-item-${item}`
+    })
+
+    this.carouselData.slice(0, 5).forEach((data, index) => {
+      const wrapper = document.querySelector(`.carousel-item-${index + 1}`)
+      wrapper.querySelector('img').src = data.src
+      wrapper.querySelector('.carousel-caption').innerHTML = `<a href="${data.link}" target="_blank">${data.title}</a>`
+
+    })
+  }
+
+  next() {
+    this.carouselData.push(this.carouselData.shift())
+    this.carouselInView.unshift(this.carouselInView.pop())
+
+    this.carouselInView.forEach((item, index) => {
+      this.carouselContainer.children[index].className = `carousel-item carousel-item-${item}`
+    })
+
+    this.carouselData.slice(0, 5).forEach((data, index) => {
+      const wrapper = document.querySelector(`.carousel-item-${index + 1}`)
+      wrapper.querySelector('img').src = data.src
+      wrapper.querySelector('.carousel-caption').innerHTML = `<a href="${data.link}" target="_blank">${data.title}</a>`
+
+    })
+  }
+}
+
+const el = document.querySelector('.carousel')
+const exampleCarousel = new Carousel(el)
+exampleCarousel.mounted()
+// fim carrossel
